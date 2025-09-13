e3800265d975b011a993ec599ffb153a3f9987e2
Chethana__Broh <84779528+tenuh@users.noreply.github.com>
2025-09-14 03:16:37 +0530

    Initial commit

---
+++ commands/help.js
@@ -0,0 +1,92 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  name: "help",
+  description: "Show a list of commands",
+  usage: "/help OR /help <command>",
+  example: "/help OR /help aur",
+  category: "Utilities",
+  handler: async (ctx) => {
+    const { message } = ctx;
+    const { text } = message;
+
+    if (text.split(" ").length < 2) {
+      const commandFiles = fs
+        .readdirSync(__dirname)
+        .filter((file) => file.endsWith(".js"));
+      const commands = [];
+      for (const file of commandFiles) {
+        const command = await import(`./${file}`);
+        commands.push({
+          name: command.default.name,
+          description: command.default.description,
+          alias: command.default.alias,
+          category: command.default.category,
+        });
+      }
+
+      const categories = [];
+      for (const command of commands) {
+        if (!categories.includes(command.category)) {
+          categories.push(command.category);
+        }
+      }
+
+      let output =
+        "Here's the list of commands you can use, categorized by their category:\n\n";
+      for (const category of categories) {
+        output += `<b>${category}</b>:\n`;
+        for (const command of commands) {
+          if (command.category === category) {
+            output += `/${command.name}`;
+            if (command.alias) {
+              output += `, /${command.alias.join(", /")}`;
+            }
+            output += ` - ${command.description}\n`;
+          }
+        }
+        output += "\n";
+      }
+
+      await ctx.reply(output, { parse_mode: "HTML" });
+    } else {
+      const command = text.split(" ")[1];
+      const commandFiles = fs
+        .readdirSync(__dirname)
+        .filter((file) => file.endsWith(".js"));
+
+      const commands = [];
+      for (const file of commandFiles) {
+        const command = await import(`./${file}`);
+        commands.push(command);
+      }
+
+      const commandDetail = commands.find(
+        (cmd) =>
+          cmd.default.name === command ||
+          (cmd.default.alias && cmd.default.alias.includes(command))
+      );
+
+      if (commandDetail) {
+        let output = `*Command:* /${commandDetail.default.name}\n`;
+        output += `*Description:* ${commandDetail.default.description}\n`;
+        output += `*Usage:* \`${commandDetail.default.usage}\`\n`;
+        output += `*Example:* \`${commandDetail.default.example}\`\n\n`;
+        output += "Run /help to see all the commands";
+
+        await ctx.reply(output, { parse_mode: "MarkdownV2" });
+      } else {
+        await ctx.reply(
+          `Command <code>${command}</code> not found!\nRun /help to see all the commands.`,
+          {
+            parse_mode: "HTML",
+          }
+        );
+      }
+    }
+  },
+};
---
+++ index.js
@@ -0,0 +1,73 @@
+import { Bot, GrammyError, HttpError } from "grammy";
+import { autoQuote } from "@roziscoding/grammy-autoquote";
+import { fileURLToPath } from "url";
+import { config } from "dotenv";
+import fs from "fs";
+import path from "node:path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+if (fs.existsSync(".env")) {
+  config();
+}
+
+const botToken = process.env.BOT_TOKEN;
+if (!botToken) {
+  throw new Error("BOT_TOKEN is not set in environment variables! Exiting...");
+}
+
+async function start() {
+  const bot = new Bot(botToken);
+  bot.use(autoQuote());
+
+  const commandFilesDir = path.resolve(__dirname, "commands");
+  const commandFiles = fs
+    .readdirSync(commandFilesDir)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const command = await import(path.join(commandFilesDir, file));
+    bot.command(command.default.name, command.default.handler);
+
+    if (command.default.alias) {
+      for (const alias of command.default.alias) {
+        bot.command(alias, command.default.handler);
+      }
+    }
+  }
+
+  bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+      console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+      console.error("Could not contact Telegram:", e);
+    } else {
+      console.error("Unknown error:", e);
+    }
+  });
+
+  process.on("uncaughtException", (err) => {
+    console.error(err);
+  });
+
+  process.on("unhandledRejection", (err) => {
+    console.error(err);
+  });
+
+  process.on("SIGINT", () => {
+    console.log("Stopping...");
+    bot.stop();
+    process.exit(0);
+  });
+
+  console.log("Starting the bot...");
+  await bot.start();
+}
+
+start().catch((error) => {
+  console.error("Error occurred during bot startup:", error);
+  process.exit(1);
+});
